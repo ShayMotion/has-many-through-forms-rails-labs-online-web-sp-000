62dc3103ff7d9365efe6538ee550a1a5c3dd184b
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-03-18 23:20:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ def categories_attributes=(categories_attributes)
   categories_attributes.values.each do |category_attributes|
     if !self.categories.include?(category)
       self.post_categories.build(:category => category)
-end
- end
+          end
+       end
     end
   end
 end
